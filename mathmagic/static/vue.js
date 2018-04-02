ad8a45174021493873e88f4304800be5fd999e49
Noah Gibson <noahjg@umich.edu>
2018-04-02 14:48:32 -0400

    added division to problem generator

--- mathmagic/static/vue.js
+++ mathmagic/static/vue.js
@@ -10,7 +10,7 @@ var App = new Vue({
     a: 0,
     b: 0,
     o: 0,
-    ops: [" + ", " - ", " * "],
+    ops: [" + ", " - ", " * ", " / "],
     status: 0   // 0 if waiting for answer, 1 if correct, -1 if wrong
   },
   methods: {
@@ -19,16 +19,19 @@ var App = new Vue({
       this.user_answer = 0
       this.a = Math.floor((Math.random() * 10) + 1)
       this.b = Math.floor((Math.random() * 10) + 1)
-      this.o = Math.floor(Math.random() * 3)
+      this.o = Math.floor(Math.random() * 4)
       var op = this.ops[this.o]
-      this.problem = this.a.toString() + op + this.b.toString()
       if (op == " + ") {
         this.answer = this.a + this.b
       } else if (op == " - ") {
         this.answer = this.a - this.b
       } else if (op == " * ") {
         this.answer = this.a * this.b
+      } else if (op == " / ") {
+        this.answer = Math.floor((Math.random() * 10) + 1)
+        this.a = this.b * this.answer
       }
+      this.problem = this.a.toString() + op + this.b.toString()
     },
     check_answer: function() {
       if (this.user_answer == this.answer) this.status = 1
