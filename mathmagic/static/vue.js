f7402835c695c7bd8e85babd20cb3b60fc3570f7
twyskiel <twyskiel@umich.edu>
2018-04-05 11:56:03 -0400

    percent right, reset score, and high score

--- mathmagic/static/vue.js
+++ mathmagic/static/vue.js
@@ -14,7 +14,11 @@ $(document).ready( function() {
       ops: [" + ", " - ", " * ", " / "],
       status: 0,   // 0 if waiting for answer, 1 if correct, -1 if wrong
       score: 0,
-      opsChecked: [" + ", " - ", " * ", " / "]
+      opsChecked: [" + ", " - ", " * ", " / "],
+      numAttempted: 0,
+      numCorrect: 0,
+      percentRight: 0,
+      highScore: 0
     },
     methods: {
       new_problem: function() {
@@ -41,12 +45,22 @@ $(document).ready( function() {
           var audio = new Audio('static/correct.wav');
           audio.play();
           this.status = 1;
+          this.numCorrect += 1;
         } else {
           var audio = new Audio('static/wrong.wav');
           audio.play();
           this.status = -1;
         }
         this.score += this.status;
+        if (this.score >= this.highScore) this.highScore = this.score;
+        this.numAttempted += 1;
+        this.percentRight = (this.numCorrect/this.numAttempted) * 100;
+      },
+      reset_score: function() {
+        this.score = 0;
+        this.numAttempted = 0;
+        this.percentRight = 0;
+        this.status = 0;
       }
     }
   })
